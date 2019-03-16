d83fe7534bcad3ebd95345c3acec7ecde7f2f0db
Jackson Seal <sealj553@gmail.com>
2019-03-15 21:00:01 -0400

    added joystick_drive to package, fixed compiler warning for statuslight

--- src/trinity/src/statuslight/statuslight.cpp
+++ src/trinity/src/statuslight/statuslight.cpp
@@ -8,7 +8,7 @@ const char *nodeName = "statuslight";
 const int lightpin = 2;
 const int delayms = 400;
 
-const char maxBrightness = 128;
+const int maxBrightness = 128;
 
 int main(int argc, char* argv[]){
     ros::init(argc, argv, nodeName);
@@ -20,13 +20,13 @@ int main(int argc, char* argv[]){
     set_mode(0, lightpin, PI_OUTPUT);  
     set_PWM_dutycycle(0, lightpin, maxBrightness);
 
-    unsigned char brightness = 0;
+    int brightness = 0;
     char increm = 1;
 
     while(ros::ok()){
         set_PWM_dutycycle(0, lightpin, brightness);
         brightness += increm;
-        if(brightness == maxBrightness - 15){
+        if(brightness == (maxBrightness - 15)){
             increm = -1;
         } else if(brightness == 5){
             increm = 1;
