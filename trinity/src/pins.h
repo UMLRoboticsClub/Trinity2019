be00d0a37dad92e063531fb45f7a212c1e91e6fb
Jackson Seal <sealj553@gmail.com>
2019-03-25 23:52:36 -0400

    added proposed GPIO locations for sensors

--- trinity/src/pins.h
+++ trinity/src/pins.h
@@ -1,34 +1,32 @@
 #pragma once
 
 //motor A
-#define MOTORA_PINA 1
-#define MOTORA_PINB 2
-#define MOTORA_ENCA 3
-#define MOTORA_ENCB 4
+#define MOTORA_PINA 4  /*1*/
+#define MOTORA_PINB 17 /*2*/
+#define MOTORA_ENCA 27 /*3*/
+#define MOTORA_ENCB 22 /*4*/
 //motor B
-#define MOTORB_PINA 5
-#define MOTORB_PINB 6
-#define MOTORB_ENCA 7
-#define MOTORB_ENCB 8
+#define MOTORB_PINA 10 /*5*/
+#define MOTORB_PINB 9  /*6*/
+#define MOTORB_ENCA 11 /*7*/
+#define MOTORB_ENCB 5  /*8*/
 //motor C
-#define MOTORC_PINA 9
-#define MOTORC_PINB 10
-#define MOTORC_ENCA 11
-#define MOTORC_ENCB 12
+#define MOTORC_PINA 6  /*9*/
+#define MOTORC_PINB 13 /*10*/
+#define MOTORC_ENCA 19 /*11*/
+#define MOTORC_ENCB 26 /*12*/
 //ir sensor
-#define IR_SENSOR   13
-//imu - not used here
-//but for reference
-#define IMU_SCL     14
-#define IMU_SDA     15
+#define IR_SENSOR   18 /*13*/
+//I2C for IMU and color sensor for reference
+//#define SDA       2  /*14*/
+//#define SCL       3  /*15*/
 //co2 solenoid
-#define SOLENOID    16
+#define SOLENOID    23 /*16*/
 //LEDs
-#define LED_STATUS  17
-#define LED_FIRE    18
-#define LED_VIDEO   19
-#define LED_SND_ACT 20
+#define LED_STATUS  24 /*17*/
+#define LED_FIRE    25 /*18*/
+#define LED_VIDEO   8  /*19*/
+#define LED_SND_ACT 7  /*20*/
+//unused pins: 12, 16, 20, 21
 
 //TODO: add sound activation script to project
-
-//color sensor?
