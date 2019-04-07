ca3928c4594a2b2214f9d00d7f4ab96370b4ac23
UML Robotics Club <uml-roboticsclub@gmail.com>
2019-04-07 20:03:47 +0000

    encoders -> odom

--- trinity_pi/src/encoders/encoders_i2c.cpp
+++ trinity_pi/src/encoders/encoders_i2c.cpp
@@ -5,6 +5,7 @@ A user-space program to get data from an I2C device.
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
+#include <cstdint>
 #include <stdlib.h>
 #include <unistd.h>
 #include <linux/i2c-dev.h>
@@ -12,138 +13,114 @@ A user-space program to get data from an I2C device.
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
- 
-char buf[10];
-extern int com_serial;
-extern int failcount;
+#include <iostream>
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+
+using std::string;
+
+char buf[12];
+
+union Encoders{
+    uint8_t b[12];
+    int32_t enc[3];
+};
  
 //////////
 // Init I2Cdevice
 //////////
-int i2c_init(char filename[40], int addr)
-    {
-        int file;
+int i2c_init(const char* filename, int addr) {
+    int file;
  
-        if ((file = open(filename,O_RDWR)) < 0)
-        {
-            printf("Failed to open the bus.");
-            /* ERROR HANDLING; you can check errno to see what went wrong */
-        com_serial=0;
-            exit(1);
-            }
- 
-    if (ioctl(file,I2C_SLAVE,addr) < 0)
-        {
-            printf("Failed to acquire bus access and/or talk to slave.\n");
-            /* ERROR HANDLING; you can check errno to see what went wrong */
-        com_serial=0;
-            exit(1);
-            }
-    return file;
+    if ((file = open(filename,O_RDWR)) < 0) {
+        printf("Failed to open the bus.");
+        /* ERROR HANDLING; you can check errno to see what went wrong */
+        exit(1);
     }
+ 
+    if (ioctl(file,I2C_SLAVE,addr) < 0) {
+        printf("Failed to acquire bus access and/or talk to slave.\n");
+        /* ERROR HANDLING; you can check errno to see what went wrong */
+        exit(1);
+    }
+    return file;
+}
  
  
 //////////
 // Set pointer address
 //////////
-void i2c_set_pointer(int address,int value,int file)
-    {
-    
-    //printf("end: 0x%x%x\n", address,value);
-    if (i2c_smbus_write_byte_data(file, address, value)<0)
-    {
-        fprintf(stderr, "Warning - write failed\n");
-    } 
-    
-    /*char buf[10];
+void i2c_set_pointer(int address, int file) {   
+    char buf[10];
     buf[0]=address;
-    buf[1]=value;
- 
-    if (write(file, buf, 2) != 2)
-        {
+    
+    int ret = write(file, buf, 1);
+    if (ret != 1) {
         fprintf(stderr, "Error setting pointer\n");
-        com_serial=0;
-        failcount++;
-        } 
-    else
-        {
+    } else {
         //printf("w_0x%0*x\n", 2, buf[0]);
-        //printf("w_0x%0*x\n", 2, buf[1]);
-        com_serial=1;
-        failcount=0;
-        }*/
- 
     }
+}
  
 //////////
 // Read n bytes
 //////////
- 
-char * i2c_read(int add1, int add2, int nbytes,int file)
-    {
+char * i2c_read(int add1, int nbytes,int file) {
     int n;
+    char* fail = "fail";
  
-    i2c_set_pointer(add1,add2,file);
- 
-    char* data = i2c_smbus_read_word_data(file, add2);
-        /*{
+    i2c_set_pointer(add1,file);
+    
+    int ret = read(file, buf, nbytes);
+    if(ret != nbytes) {
         fprintf(stderr, "Error reading %i bytes\n",nbytes);
-        com_serial=0;
-        failcount++;
-        } 
-    else
-        {
+    } else {
         for (n=0;n<nbytes;n++)
-            {
             //printf("r_0x%0*x\n", 2, buf[n]);
-            }
-        com_serial=1;
-        failcount=0;
         return buf;
-        }*/
-	return data;
     }
- 
-//////////
-// Write n bytes
-//////////
-void i2c_write(int add1,int add2,int nbytes,char value[10],int file)
-    {
-    int n;
-    unsigned char buf[10];
-    buf[0] = add1;
-    buf[1] = add2;
-    if (nbytes>=1) buf[2] = value[0];
-    if (nbytes>=2) buf[3] = value[1];
-    if (nbytes>=3) buf[4] = value[2];
-    if (nbytes>=4) buf[5] = value[3];
- 
-    if (write(file, buf, nbytes) != nbytes)
-        {
-        fprintf(stderr, "Error writing %i bytes\n",nbytes);
-        com_serial=0;
-        failcount++;
-        } 
-    else
-        {
-        for (n=0;n<(nbytes+2);n++)
-            {
-            //printf("w_0x%0*x\n", 2, buf[n]);
-            }
-        com_serial=1;
-        failcount=0;
-        }
-    }
+    return fail;
+}
 
 int main(int argc, char** argv){
-	char *buffer
-	char buf[12];
+    ros::init(argc, argv, "encoders");
+    ros::NodeHandle n;
+    string topic_nameA = "enc1", topic_nameB = "enc2", topic_nameC = "enc3", i2c_device = "/dev/i2c-1";
+    int slave_address = 0x03, file, pub_hz = 20;
+    char* buffer;
+    Encoders enc;
 
-	file = i2c_init("/dev/i2c-1", 0xAA);
+    n.getParam("encoder1", topic_nameA);
+    n.getParam("encoder2", topic_nameB);
+    n.getParam("encoder3", topic_nameC);
+    n.getParam("i2c_device", i2c_device);
+    n.getParam("slave_address", slave_address);
+    n.getParam("pub_hz", pub_hz);
+	file = i2c_init(i2c_device.c_str(), slave_address);
+    ROS_INFO("Opened i2c bus on address %x", slave_address);
+    //printf("%s, %d\n", i2c_device, slave_address);
 
-	buffer = (char*) i2c_read(0x00, 0x0E, 12, file); //read at address 0xE60E
-
-	printf("%s\n", buffer);
-
+    ros::Publisher pubA = n.advertise<std_msgs::Int32>(topic_nameA, 1000);
+    ros::Publisher pubB = n.advertise<std_msgs::Int32>(topic_nameB, 1000);
+    ros::Publisher pubC = n.advertise<std_msgs::Int32>(topic_nameC, 1000);
+    ros::Rate loop_rate(pub_hz);
+    std_msgs::Int32 c;
+    
+    ROS_INFO("Publishing encoder values");
+    while(ros::ok()){
+        buffer = i2c_read(0x05, 12, file); //read at address 0x05
+        for(int i = 0; i < 12; i++){
+            enc.b[i] = buffer[i];
+        }
+        c.data = enc.enc[0];
+        pubA.publish(c);
+        c.data = enc.enc[1];
+        pubB.publish(c);
+        c.data = enc.enc[2];
+        pubC.publish(c);
+        //printf("%d, %d, %d\n", enc.enc[0], enc.enc[1], enc.enc[2]);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 	close(file);
 }
