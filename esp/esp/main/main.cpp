4c21a91435291b3bbe364268b9667ae80b95fcfd
Jackson Seal <sealj553@gmail.com>
2020-01-12 18:50:22 -0500

    cleaned up example and increased upload speed

--- esp/esp/main/main.cpp
+++ esp/esp/main/main.cpp
@@ -13,16 +13,24 @@ ros::Publisher chatter("chatter", &str_msg);
 char hello[13] = "hello world!";
 
 void rosserial_setup(){
-      // Initialize ROS
-      nh.initNode();
-        nh.advertise(chatter);
+    // Initialize ROS
+    nh.initNode();
+    nh.advertise(chatter);
 }
 
 void rosserial_publish(){
-      str_msg.data = hello;
-        // Send the message
-        chatter.publish(&str_msg);
-          nh.spinOnce();
+    str_msg.data = hello;
+    // Send the message
+    chatter.publish(&str_msg);
+    nh.spinOnce();
+}
+
+void rosserial_spinonce(){
+    nh.spinOnce();
+}
+
+void delay_ms(size_t ms){
+    vTaskDelay(ms / portTICK_PERIOD_MS);
 }
 
 extern "C" {
@@ -30,8 +38,8 @@ extern "C" {
         rosserial_setup();
 
         while(1){
-            nh.spinOnce();
-            vTaskDelay(100);
+            rosserial_spinonce();
+            delay_ms(1000);
         }
     }
 }
